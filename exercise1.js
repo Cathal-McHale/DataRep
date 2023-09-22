af3c9d79748d4bc028df902a2880f6cbcaf5fc87
Cathalmchale3 <Cathalmchale14@gmail.com>
2023-09-22 11:18:04 +0000

    first

---
+++ exercise1.js
@@ -0,0 +1,27 @@
+
+console.log("Hello world!");
+
+function SayHello(){
+    console.log("Hello");
+}
+
+SayHello();
+
+const myFunction = function SayHi(){
+    console.log("hi");
+}
+
+myFunction();
+
+const myNewFunction = function(){
+    console.log("Bonjour");
+}
+
+myNewFunction();
+
+const myComponent = ()=>{
+    console.log("Bye");
+}
+
+myComponent();
+
